bcbbe330d61ae6f83433cc198dbe5a7ca0f8342e
hakima3 <hakima.merzayee3@gmail.com>
2022-11-23 16:36:49 +0430

    add changes

--- pages/start.vue
+++ pages/start.vue
@@ -141,7 +141,6 @@ export default {
         number: null,
         city: null,
         area: null,
-        address_id: null,
         address: null,
         products: [
           {
@@ -151,6 +150,7 @@ export default {
             product_quantity: null,
             product_size: null,
             product_color: "#FF0000FF",
+            product_price: null,
           },
         ],
 
@@ -193,6 +193,16 @@ export default {
       project: { required },
       country: { required },
       no_ad_order: {},
+      products: {
+        $each: {
+          id: {},
+          product_code: { required },
+          product_quantity: { required },
+          product_size: { required },
+          product_color: { required },
+          product_price: { required },
+        },
+      },
     },
   },
   methods: {
