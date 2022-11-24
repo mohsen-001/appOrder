28b752244bccec6ab030d405e1d8ff3b0e3ef9de
HajiRohullah <rohullahhussaini67@gmail.com>
2022-11-24 15:35:05 +0430

    save changes

--- pages/startPage.vue
+++ pages/startPage.vue
@@ -1,13 +1,13 @@
 <template>
-    <div>
-        <h2>start page</h2>
-    </div>
+  <div>
+    <h2>start page</h2>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'OrderLoginPage'
-}
+  name: "OrderLoginPage",
+};
 </script>
 
 
